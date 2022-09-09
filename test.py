d1a5e5fb27ed7d5a9674c9709015fa576dd605d6
Yan Savitskiy <yan-sav@yandex.ru>
2022-09-09 13:04:44 +0300

    First commit

---
+++ test.py
@@ -0,0 +1,44 @@
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None],[]
+]
+
+class FlatIterator():
+    cursor = 0
+    cursor_index = -1
+    def __init__(self,collection) -> None:
+       self.collection = collection
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.cursor_index += 1
+        while self.cursor_index > len(self.collection[self.cursor]) - 1:
+            self.cursor += 1
+            self.cursor_index = 0
+            if self.cursor > len(self.collection) - 1:
+                raise StopIteration            
+        return self.collection[self.cursor][self.cursor_index]
+                
+      
+for i in FlatIterator(nested_list):
+    print(i)
+
+flat_list = [item for item in FlatIterator(nested_list)]
+print(flat_list)
+
+def my_next(nested_list):
+    cursor = 0
+    cursor_index = -1
+    cursor_index += 1
+    while cursor > len(nested_list) - 1:
+        cursor +=1
+        cursor_index = 0
+        if cursor_index > len(nested_list) - 1:
+            break
+        yield nested_list[cursor][cursor_index]
+
+for item in  my_next(nested_list):
+	print(item)
